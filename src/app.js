67d6a9b3118e1533f5b97c5b0d7f27840ab73a8a
Fabio Penna <ffmpenna@gmail.com>
2023-01-15 21:02:35 -0300

    feat: cria rotas e comunicação entre as camadas

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const productsRouter = require('./routers/productsRouter');
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/products', productsRouter);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
-
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
+
 module.exports = app;
