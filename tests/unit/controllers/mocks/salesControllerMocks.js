decda1d2abada85bae366eddd2f185536b1ebb90
Fabio Penna <ffmpenna@gmail.com>
2023-01-18 00:20:19 -0300

    feat: cria os testes de listagem de vendas da camada controller

--- tests/unit/controllers/mocks/salesControllerMocks.js
+++ tests/unit/controllers/mocks/salesControllerMocks.js
@@ -23,6 +23,40 @@ const saleCreateResponse = {
   ],
 };
 
+const allSalesResponse = [
+  {
+    saleId: 1,
+    date: '2021-09-09T04:54:29.000Z',
+    productId: 1,
+    quantity: 2,
+  },
+  {
+    saleId: 1,
+    date: '2021-09-09T04:54:54.000Z',
+    productId: 2,
+    quantity: 2,
+  },
+  {
+    saleId: 2,
+    date: '2021-10-09T04:54:54.000Z',
+    productId: 3,
+    quantity: 15,
+  },
+];
+
+const saleResponse = [
+  {
+    date: '2021-09-09T04:54:29.000Z',
+    productId: 1,
+    quantity: 2,
+  },
+  {
+    date: '2021-09-09T04:54:54.000Z',
+    productId: 2,
+    quantity: 2,
+  },
+];
+
 module.exports = {
   wrongSaleNotProductIdBody,
   wrongSaleNotQuantityBody,
@@ -33,4 +67,6 @@ module.exports = {
   otherProductIdSaleBody,
   rightSaleBody,
   saleCreateResponse,
+  allSalesResponse,
+  saleResponse,
 };
--- tests/unit/controllers/salesControllers.test.js
+++ tests/unit/controllers/salesControllers.test.js
@@ -1,22 +1,23 @@
-const chai = require("chai");
-const sinon = require("sinon");
-const sinonChai = require("sinon-chai");
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 
 const { expect } = chai;
 chai.use(sinonChai);
 
-const { salesService } = require("../../../src/services");
-const { salesController } = require("../../../src/controllers");
+const { salesService } = require('../../../src/services');
+const { salesController } = require('../../../src/controllers');
 const {
   rightSaleBody,
   saleCreateResponse,
   wrongSaleNotProductIdBody,
   wrongSaleNotQuantityBody,
-} = require("./mocks/salesControllerMocks");
+  allSalesResponse, saleResponse,
+} = require('./mocks/salesControllerMocks');
 
-describe("Teste de unidade do salesController.", () => {
-  describe("Cadastrar novas vendas com valores válidos.", () => {
-    it("Deve retornar o status 201 e os dados da venda.", async () => {
+describe('Teste de unidade do salesController.', () => {
+  describe('Cadastrar novas vendas com valores válidos.', () => {
+    it('Deve retornar o status 201 e os dados da venda.', async () => {
       const res = {};
       const req = {
         body: rightSaleBody,
@@ -25,7 +26,7 @@ describe("Teste de unidade do salesController.", () => {
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns();
       sinon
-        .stub(salesService, "create")
+        .stub(salesService, 'create')
         .resolves({ type: null, message: saleCreateResponse });
 
       await salesController.createSale(req, res);
@@ -34,7 +35,7 @@ describe("Teste de unidade do salesController.", () => {
       expect(res.json).to.have.been.calledWith(saleCreateResponse);
     });
   });
-  describe("Cadastrar novas vendas com valores inválidos.", () => {
+  describe('Cadastrar novas vendas com valores inválidos.', () => {
     it("Se algum item da requisição não tiver o campo 'productId' deve retornar status 400", async () => {
       const res = {};
       const req = {
@@ -44,8 +45,8 @@ describe("Teste de unidade do salesController.", () => {
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns();
       sinon
-        .stub(salesService, "create")
-        .resolves({ type: "BAD_REQUEST", message: "'productId' is required" });
+        .stub(salesService, 'create')
+        .resolves({ type: 'BAD_REQUEST', message: "'productId' is required" });
 
       await salesController.createSale(req, res);
 
@@ -63,8 +64,8 @@ describe("Teste de unidade do salesController.", () => {
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns();
       sinon
-        .stub(salesService, "create")
-        .resolves({ type: "BAD_REQUEST", message: "'quantity' is required" });
+        .stub(salesService, 'create')
+        .resolves({ type: 'BAD_REQUEST', message: "'quantity' is required" });
 
       await salesController.createSale(req, res);
 
@@ -74,6 +75,60 @@ describe("Teste de unidade do salesController.", () => {
       });
     });
   });
+  describe('Listar todas as vendas', () => {
+    it('Deve retornar status 200 e a lista de vendas', async () => {
+      const res = {};
+      const req = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(salesServices, 'getAll')
+        .resolves({ type: null, message: allSalesResponse });
+
+      await salesController.listSales(req, res);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(allSalesResponse);
+    });
+  });
+  describe('Listar venda por Id', () => {
+    it('Deve retornar status 200 e os dados da venda', async () => {
+      const res = {};
+      const req = {
+        params: { id: 1 }
+      };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(salesServices, 'getById')
+        .resolves({ type: null, message: saleResponse });
+
+      await salesController.listSales(req, res);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(saleResponse);
+    });
+    it('Caso o id não exista deve retornar um erro', async () => {
+       const res = {};
+      const req = {
+        params: { id: 'invalid_value' }
+      };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(salesServices, 'getById')
+        .resolves({ type: 'SALE_NOT_FOUND', message: 'Sale not found' });
+
+      await salesController.listSales(req, res);
+
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.json).to.have.been.calledWith({ message: 'Sale not found' });
+    })
+  });
+
   afterEach(() => {
     sinon.restore();
   });
