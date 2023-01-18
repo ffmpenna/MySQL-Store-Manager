a5058d6b986231216c8d7cb11528534ae35ebfdc
Fabio Penna <ffmpenna@gmail.com>
2023-01-17 23:54:03 -0300

    feat: implementa as validações dentro da função de criar nova venda

--- src/services/salesService.js
+++ src/services/salesService.js
@@ -1,8 +1,14 @@
-const salesModel = require('../models/salesModel');
+const { salesModel } = require('../models');
 const newSaleFormatter = require('../utils/newSaleFormatter');
+const schema = require('./validations/validationsInputValues');
 
-const create = async (salesData) => {
-  const newSaleId = await salesModel.insert(salesData);
+const create = async (saleData) => {
+  let error = schema.validateSalesFields(saleData);
+  if (error.type) return error;
+  error = await Promise.resolve(schema.validateSalesInputs(saleData));
+  if (error.type) return error;
+
+  const newSaleId = await salesModel.insert(saleData);
   const newSale = await salesModel.getById(newSaleId);
 
   return { type: null, message: newSaleFormatter(newSaleId, newSale) };
