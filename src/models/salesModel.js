86a8687cfa58a6259d5b4e0f0adb5021b2136f18
Fabio Penna <ffmpenna@gmail.com>
2023-01-17 21:08:00 -0300

    style: nome de variáveis e indentação

--- src/models/salesModel.js
+++ src/models/salesModel.js
@@ -2,10 +2,9 @@ const camelize = require('camelize');
 const connection = require('./connection');
 
 const getById = async (saleId) => {
-  const query = `SELECT sp.product_id, sp.quantity, s.date FROM
-    StoreManager.sales_products AS sp
-        INNER JOIN
-    StoreManager.sales AS s
+  const query = `SELECT sp.product_id, sp.quantity, s.date 
+  FROM StoreManager.sales_products AS sp
+  INNER JOIN StoreManager.sales AS s
     ON s.id = sp.sale_id
     WHERE s.id = ?;`;
 
@@ -17,12 +16,12 @@ const insert = async (saleData) => {
   const queryIntoSales = 'INSERT INTO StoreManager.sales (date) VALUES (NOW())';
   const [newSale] = await connection.execute(queryIntoSales);
 
-  const saleProducts = saleData
+  const soldProducts = saleData
     .map(({ productId, quantity }) => `(${newSale.insertId}, ${productId}, ${quantity})`)
     .join(', ');
 
   await connection.execute(
-    `INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES ${saleProducts
+    `INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES ${soldProducts
     }`,
   );
 
