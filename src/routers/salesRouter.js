4a551f34cab1264c651614d4433dad908a6b542a
Fabio Penna <ffmpenna@gmail.com>
2023-01-18 01:39:12 -0300

    feat: implementa as rotas get para as vendas

--- src/routers/salesRouter.js
+++ src/routers/salesRouter.js
@@ -6,4 +6,8 @@ const router = express.Router();
 
 router.post('/', validateNewSaleFields, salesController.createSale);
 
+router.get('/', salesController.listSales);
+
+router.get('/:id', salesController.getSale);
+
 module.exports = router;
