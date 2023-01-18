a8c58d7f2fa911ad99e4091d6cc7edab523a4fc2
Fabio Penna <ffmpenna@gmail.com>
2023-01-17 23:54:37 -0300

    feat: implementa middleware de validação de campo da requisição na rota de criar uma venda

--- src/routers/salesRouter.js
+++ src/routers/salesRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const salesController = require('../controllers/salesController');
+const validateNewSaleFields = require('../middlewares/validateNewSaleFields');
 
 const router = express.Router();
 
-router.post('/', salesController.createSale);
+router.post('/', validateNewSaleFields, salesController.createSale);
 
 module.exports = router;
